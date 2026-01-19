dd03b0b5b25b1adaa0b86b90e3b36c82e0d380b1
gowdanavya97 <128294280+gowdanavya97@users.noreply.github.com>
2026-01-19 13:31:22 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-02b8269d5e85954ef"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
